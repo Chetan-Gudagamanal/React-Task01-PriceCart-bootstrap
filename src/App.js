6ad949420ae348ff2a0b2fd5082bdc48d4a8d1cd
Chetan-Gudagamanal <chetanbg95@gmail.com>
2021-06-16 23:28:36 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 import Card from "./Card"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+let tierArr=[
+  {"tier":"FREE","cost":"0","user":"Single User","storage":"5GB Storage","public-projects":"Unlimited Public Projects","access":"Community Access"},
+  {"tier":"PLUS","cost":"9","user":"5 Users","storage":"50GB Storage","public-projects":"Unlimited Public Projects","access":"Community Access","private-projects":"Unlimited Private Projects","phone-support":"Dedicated Phone Support","subdomain":"Free Subdomain"},
+  {"tier":"PRO","cost":"49","user":"Unlimited Users","storage":"150GB Storage","public-projects":"Unlimited Public Projects","access":"Community Access","private-projects":"Unlimited Private Projects","phone-support":"Dedicated Phone Support","subdomain":"Free Subdomain","reports":"Monthly Status Reports"}
+]
 function App() {
   return (
-    // <div classNameName="App">
       <section className="pricing py-5">
-      <div className="container">
-        <div className="row">
-          <Card tier={"FREE"} cost={"0"}/>
-          <Card tier={"PLUS"} cost={"9"}/>
-          <Card tier={"PRO"} cost={"49"}/>
+        <div className="container">
+          <div className="row">
+            <Card tierArr={tierArr}/>
+          </div>
         </div>
-      </div>
       </section>
-    // </div>
   );
 }
+
 
 export default App;
--- src/CardDetails.js
+++ src/CardDetails.js
@@ -1,45 +1,29 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
-export default function CardDetails({tier}){
-    if(tier==="FREE"){
+
+export default function CardDetails({tierObj}){
+    
         return(
             <ul className="fa-ul">
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Single User</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>5GB Storage</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-                <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Private Projects</li>
-                <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated Phone Support</li>
-                <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain</li>
-                <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
+                <li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["user"]}</li>
+                <li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["storage"]}</li>
+                <li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["public-projects"]}</li>
+                <li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["access"]}</li>
+                
+                {tierObj["private-projects"]?
+                (<li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["private-projects"]}</li>):
+                (<li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Unlimited Private Projects</li>)}
+
+                {tierObj["phone-support"]?
+                (<li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["phone-support"]}</li>):
+                (<li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Dedicated Phone Support</li>)}
+
+                {tierObj["subdomain"]?
+                (<li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["subdomain"]}</li>):
+                (<li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Free Subdomain</li>)}
+
+                {tierObj["reports"]?
+                (<li><span className="fa-li"><i className="fas fa-check"></i></span>{tierObj["reports"]}</li>):
+                (<li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>)}
             </ul>
         )
     }
-    else if(tier==="PLUS"){
-        return(
-            <ul className="fa-ul">
-                <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>5 Users</strong></li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>50GB Storage</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Free Subdomain</li>
-                <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span>Monthly Status Reports</li>
-            </ul>
-        )
-    }
-    else{
-        return(
-            <ul className="fa-ul">
-                <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited Users</strong></li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>150GB Storage</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Community Access</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Unlimited Private Projects</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Dedicated Phone Support</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span><strong>Unlimited</strong> Free Subdomains</li>
-                <li><span className="fa-li"><i className="fas fa-check"></i></span>Monthly Status Reports</li>
-            </ul>
-        )
-    }
-}
